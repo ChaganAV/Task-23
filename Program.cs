7e6ed5148ff387486746fc3af22fbe6d68dc12be
Alexandr <jwchaganav@gmail.com>
2022-10-22 15:22:03 +0400

    end

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while(count <= num)
+{
+    //Console.WriteLine($"{count} в кубе = {count*count*count}");
+    if (count == num)
+        Console.Write($"{count*count*count}");
+    else
+        Console.Write($"{count*count*count}, ");
+    count++;
+}
